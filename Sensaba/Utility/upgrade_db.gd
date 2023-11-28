483a76c6568969880b29305dce9bf6f1d4d850f1
モナックマ <kimiko414141@gmail.com>
2023-11-28 11:11:51 +0900

    翻訳追加

--- Sensaba/Utility/upgrade_db.gd
+++ Sensaba/Utility/upgrade_db.gd
@@ -6,248 +6,248 @@ const WEAPON_PATH = "res://Textures/Items/Weapons/"
 const UPGRADES = {
 	"icespear1": {
 		"icon": WEAPON_PATH + "ice_spear.png",
-		"displayname": "Ice Spear",
-		"details": "A spear of ice is thrown at a random enemy",
+		"displayname": "木の矢",
+		"details": "ランダムに敵を攻撃する",
 		"level": "Level: 1",
 		"prerequisite": [],
 		"type": "weapon"
 	},
 	"icespear2": {
 		"icon": WEAPON_PATH + "ice_spear.png",
-		"displayname": "Ice Spear",
-		"details": "An addition Ice Spear is thrown",
+		"displayname": "木の矢",
+		"details": "追加で木の矢をうつ",
 		"level": "Level: 2",
 		"prerequisite": ["icespear1"],
 		"type": "weapon"
 	},
 	"icespear3": {
 		"icon": WEAPON_PATH + "ice_spear.png",
-		"displayname": "Ice Spear",
-		"details": "Ice Spears now pass through another enemy and do + 3 damage",
+		"displayname": "木の矢",
+		"details": "木の矢をさらに追加とダメージ＋３",
 		"level": "Level: 3",
 		"prerequisite": ["icespear2"],
 		"type": "weapon"
 	},
 	"icespear4": {
 		"icon": WEAPON_PATH + "ice_spear.png",
-		"displayname": "Ice Spear",
-		"details": "An additional 2 Ice Spears are thrown",
+		"displayname": "木の矢",
+		"details": "さらに2本矢を追加する",
 		"level": "Level: 4",
 		"prerequisite": ["icespear3"],
 		"type": "weapon"
 	},
 	"javelin1": {
 		"icon": WEAPON_PATH + "javelin_3_new_attack.png",
-		"displayname": "Javelin",
-		"details": "A magical javelin will follow you attacking enemies in a straight line",
+		"displayname": "魔槍",
+		"details": "直線上に飛んでいって敵を攻撃する",
 		"level": "Level: 1",
 		"prerequisite": [],
 		"type": "weapon"
 	},
 	"javelin2": {
 		"icon": WEAPON_PATH + "javelin_3_new_attack.png",
-		"displayname": "Javelin",
-		"details": "The javelin will now attack an additional enemy per attack",
+		"displayname": "魔槍",
+		"details": "攻撃回数が追加",
 		"level": "Level: 2",
 		"prerequisite": ["javelin1"],
 		"type": "weapon"
 	},
 	"javelin3": {
 		"icon": WEAPON_PATH + "javelin_3_new_attack.png",
-		"displayname": "Javelin",
-		"details": "The javelin will attack another additional enemy per attack",
+		"displayname": "魔槍",
+		"details": "敵をさらに攻撃する",
 		"level": "Level: 3",
 		"prerequisite": ["javelin2"],
 		"type": "weapon"
 	},
 	"javelin4": {
 		"icon": WEAPON_PATH + "javelin_3_new_attack.png",
-		"displayname": "Javelin",
-		"details": "The javelin now does + 5 damage per attack and causes 20% additional knockback",
+		"displayname": "魔槍",
+		"details": "ノックバック20％増加＋５ダメージ追加",
 		"level": "Level: 4",
 		"prerequisite": ["javelin3"],
 		"type": "weapon"
 	},
 	"tornado1": {
 		"icon": WEAPON_PATH + "tetusenn.png",
-		"displayname": "Tornado",
-		"details": "A tornado is created and random heads somewhere in the players direction",
+		"displayname": "鉄扇",
+		"details": "敵にランダムに飛んでいく",
 		"level": "Level: 1",
 		"prerequisite": [],
 		"type": "weapon"
 	},
 	"tornado2": {
 		"icon": WEAPON_PATH + "tetusenn.png",
-		"displayname": "Tornado",
-		"details": "An additional Tornado is created",
+		"displayname": "鉄扇",
+		"details": "追加の鉄扇を飛ばす",
 		"level": "Level: 2",
 		"prerequisite": ["tornado1"],
 		"type": "weapon"
 	},
 	"tornado3": {
 		"icon": WEAPON_PATH + "tetusenn.png",
-		"displayname": "Tornado",
-		"details": "The Tornado cooldown is reduced by 0.5 seconds",
+		"displayname": "鉄扇",
+		"details": "05秒早く飛ばせる",
 		"level": "Level: 3",
 		"prerequisite": ["tornado2"],
 		"type": "weapon"
 	},
 	"tornado4": {
 		"icon": WEAPON_PATH + "tetusenn.png",
-		"displayname": "Tornado",
-		"details": "An additional tornado is created and the knockback is increased by 25%",
+		"displayname": "鉄扇",
+		"details": "鉄扇を追加で飛ばすのとノックバック２５％",
 		"level": "Level: 4",
 		"prerequisite": ["tornado3"],
 		"type": "weapon"
 	},
 	"armor1": {
 		"icon": ICON_PATH + "yoroi.png",
-		"displayname": "Armor",
-		"details": "Reduces Damage By 1 point",
+		"displayname": "鎧",
+		"details": "１ダメージ軽減",
 		"level": "Level: 1",
 		"prerequisite": [],
 		"type": "upgrade"
 	},
 	"armor2": {
 		"icon": ICON_PATH + "yoroi.png",
-		"displayname": "Armor",
-		"details": "Reduces Damage By an additional 1 point",
+		"displayname": "鎧",
+		"details": "追加で１ダメージ軽減",
 		"level": "Level: 2",
 		"prerequisite": ["armor1"],
 		"type": "upgrade"
 	},
 	"armor3": {
 		"icon": ICON_PATH + "yoroi.png",
-		"displayname": "Armor",
-		"details": "Reduces Damage By an additional 1 point",
+		"displayname": "鎧",
+		"details": "追加で１ダメージ軽減",
 		"level": "Level: 3",
 		"prerequisite": ["armor2"],
 		"type": "upgrade"
 	},
 	"armor4": {
 		"icon": ICON_PATH + "yoroi.png",
-		"displayname": "Armor",
-		"details": "Reduces Damage By an additional 1 point",
+		"displayname": "鎧",
+		"details": "追加で１ダメージ軽減",
 		"level": "Level: 4",
 		"prerequisite": ["armor3"],
 		"type": "upgrade"
 	},
 	"speed1": {
 		"icon": ICON_PATH + "zouri.png",
-		"displayname": "Speed",
-		"details": "Movement Speed Increased by 50% of base speed",
+		"displayname": "草履",
+		"details": "移動スピード50％アップ",
 		"level": "Level: 1",
 		"prerequisite": [],
 		"type": "upgrade"
 	},
 	"speed2": {
 		"icon": ICON_PATH + "zouri.png",
-		"displayname": "Speed",
-		"details": "Movement Speed Increased by an additional 50% of base speed",
+		"displayname": "草履",
+		"details": "移動スピード50％アップ",
 		"level": "Level: 2",
 		"prerequisite": ["speed1"],
 		"type": "upgrade"
 	},
 	"speed3": {
 		"icon": ICON_PATH + "zouri.png",
-		"displayname": "Speed",
-		"details": "Movement Speed Increased by an additional 50% of base speed",
+		"displayname": "草履",
+		"details": "移動スピード50％アップ",
 		"level": "Level: 3",
 		"prerequisite": ["speed2"],
 		"type": "upgrade"
 	},
 	"speed4": {
 		"icon": ICON_PATH + "zouri.png",
-		"displayname": "Speed",
-		"details": "Movement Speed Increased an additional 50% of base speed",
+		"displayname": "草履",
+		"details": "移動スピード50％アップ",
 		"level": "Level: 4",
 		"prerequisite": ["speed3"],
 		"type": "upgrade"
 	},
 	"tome1": {
 		"icon": ICON_PATH + "thick_new.png",
-		"displayname": "Tome",
-		"details": "Increases the size of spells an additional 10% of their base size",
+		"displayname": "本",
+		"details": "10％攻撃スピードが上がる",
 		"level": "Level: 1",
 		"prerequisite": [],
 		"type": "upgrade"
 	},
 	"tome2": {
 		"icon": ICON_PATH + "thick_new.png",
-		"displayname": "Tome",
-		"details": "Increases the size of spells an additional 10% of their base size",
+		"displayname": "本",
+		"details": "10％攻撃スピードが上がる",
 		"level": "Level: 2",
 		"prerequisite": ["tome1"],
 		"type": "upgrade"
 	},
 	"tome3": {
 		"icon": ICON_PATH + "thick_new.png",
-		"displayname": "Tome",
-		"details": "Increases the size of spells an additional 10% of their base size",
+		"displayname": "本",
+		"details": "10％攻撃スピードが上がる",
 		"level": "Level: 3",
 		"prerequisite": ["tome2"],
 		"type": "upgrade"
 	},
 	"tome4": {
 		"icon": ICON_PATH + "thick_new.png",
-		"displayname": "Tome",
-		"details": "Increases the size of spells an additional 10% of their base size",
+		"displayname": "本",
+		"details": "10％攻撃スピードが上がる",
 		"level": "Level: 4",
 		"prerequisite": ["tome3"],
 		"type": "upgrade"
 	},
 	"scroll1": {
 		"icon": ICON_PATH + "scroll_old.png",
-		"displayname": "Scroll",
-		"details": "Decreases of the cooldown of spells by an additional 5% of their base time",
+		"displayname": "巻物",
+		"details": "クールダウンスピード５％アップ",
 		"level": "Level: 1",
 		"prerequisite": [],
 		"type": "upgrade"
 	},
 	"scroll2": {
 		"icon": ICON_PATH + "scroll_old.png",
-		"displayname": "Scroll",
-		"details": "Decreases of the cooldown of spells by an additional 5% of their base time",
+		"displayname": "巻物",
+		"details": "クールダウンスピード５％アップ",
 		"level": "Level: 2",
 		"prerequisite": ["scroll1"],
 		"type": "upgrade"
 	},
 	"scroll3": {
 		"icon": ICON_PATH + "scroll_old.png",
-		"displayname": "Scroll",
-		"details": "Decreases of the cooldown of spells by an additional 5% of their base time",
+		"displayname": "巻物",
+		"details": "クールダウンスピード５％アップ",
 		"level": "Level: 3",
 		"prerequisite": ["scroll2"],
 		"type": "upgrade"
 	},
 	"scroll4": {
 		"icon": ICON_PATH + "scroll_old.png",
-		"displayname": "Scroll",
-		"details": "Decreases of the cooldown of spells by an additional 5% of their base time",
+		"displayname": "巻物",
+		"details": "クールダウンスピード５％アップ",
 		"level": "Level: 4",
 		"prerequisite": ["scroll3"],
 		"type": "upgrade"
 	},
 	"ring1": {
 		"icon": ICON_PATH + "urand_mage.png",
-		"displayname": "Ring",
-		"details": "Your spells now spawn 1 more additional attack",
+		"displayname": "指輪",
+		"details": "今ある攻撃回数＋１",
 		"level": "Level: 1",
 		"prerequisite": [],
 		"type": "upgrade"
 	},
 	"ring2": {
 		"icon": ICON_PATH + "urand_mage.png",
-		"displayname": "Ring",
-		"details": "Your spells now spawn an additional attack",
+		"displayname": "指輪",
+		"details": "今ある攻撃回数＋１",
 		"level": "Level: 2",
 		"prerequisite": ["ring1"],
 		"type": "upgrade"
 	},
 	"food": {
 		"icon": ICON_PATH + "chunk.png",
-		"displayname": "Food",
-		"details": "Heals you for 20 health",
+		"displayname": "食べ物",
+		"details": "HPが20増える",
 		"level": "N/A",
 		"prerequisite": [],
 		"type": "item"
